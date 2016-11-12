592eaec20ca6e6a63ed283ca5f7993e558c6b2dc
Mark <mark@remarkablemark.org>
2016-11-12 20:26:53 +0000

    Add boilerplate based on `react-quickstart`
    https://github.com/remarkablemark/react-quickstart

---
+++ components/App.jsx
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+import React from 'react';
+
+/**
+ * App component.
+ */
+class App extends React.Component {
+    render() {
+        return <h1>Welcome to React Quiz</h1>;
+    }
+}
+
+export default App;
---
+++ main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App.jsx';
+
+render(
+    <App />,
+    document.getElementById('root')
+);
