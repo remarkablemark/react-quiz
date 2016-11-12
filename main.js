f2f65d4bbc30284e0452a714bfb7e07e6ac26b97
Mark <mark@remarkablemark.org>
2016-11-12 21:14:45 +0000

    Update webpack to resolve `.jsx` extension

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import App from './components/App.jsx';
+import App from './components/App';
 
 // needed for `onTouchTap`
 // https://github.com/callemall/material-ui#react-tap-event-plugin
--- webpack.config.js
+++ webpack.config.js
@@ -15,5 +15,8 @@ module.exports = {
                 }
             }
         ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx', '.json']
     }
 };
